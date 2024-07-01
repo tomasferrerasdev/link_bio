174cada6e4abaefe24064f98321e442292acb129
Tomas Ferreras <hellotomasdev@gmail.com>
2024-07-01 18:57:24 -0300

    chore: remove unused code

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -1,39 +1,19 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+""" This is a simple example of a reflex component. """
 
 import reflex as rx
 
-from rxconfig import config
-
 
 class State(rx.State):
-    """The app state."""
+    """This is a simple example of a reflex state."""
 
-    ...
+    pass
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
+    """This is a simple example of a reflex component."""
+    return rx.text("Hola reflex!", style={"color": "red"})
 
 
 app = rx.App()
 app.add_page(index)
+app._compile()
